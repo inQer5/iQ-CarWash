892c5f53a88ce3e4c186416ffd4e2668b5161353
inQer5 <145898779+inQer5@users.noreply.github.com>
2024-06-26 12:00:34 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,1 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
 
+author 'inQer'
+description 'Car Wash Script'
+version '1.0.0'
+
+shared_script '@es_extended/imports.lua'
+client_script 'client.lua'
+server_script 'server.lua'
+dependencies {
+    'es_extended',
+    'ox_lib'
+}
