2bfcf5792bf1ae2ba108da79a1c4bf1ac218dfc5
inQer5 <145898779+inQer5@users.noreply.github.com>
2024-06-26 12:01:00 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,1 +1,11 @@
+local ESX = exports['es_extended']:getSharedObject()
 
+ESX.RegisterServerCallback('carwash:pay', function(source, cb, amount)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    if xPlayer.getMoney() >= amount then
+        xPlayer.removeMoney(amount)
+        cb(true)
+    else
+        cb(false)
+    end
+end)
