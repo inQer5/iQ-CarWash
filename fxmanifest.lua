abcea4eab527e15074391c10a1b546702c532351
inQer5 <145898779+inQer5@users.noreply.github.com>
2024-06-26 14:46:49 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,8 +6,8 @@ description 'Car Wash Script'
 version '0.0.1'
 
 shared_script '@es_extended/imports.lua'
-client_script 'client.lua'
-server_script 'server.lua'
+client_script 'Client/client.lua'
+server_script 'Server/server.lua'
 dependencies {
     'es_extended',
     'ox_lib'
