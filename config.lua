b67e12aacecf2d9453acb613407f2d511a6b7277
inQer5 <145898779+inQer5@users.noreply.github.com>
2024-06-27 17:06:20 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,3 +6,13 @@ Config.Prices = {
     standard = 500,
     luxury = 1000
 }
+
+Config.OpenHours = {
+    open = 12,  -- 12:00
+    close = 18  -- 18:00
+}
+
+Config.Locations = {
+    {x = 176.13, y = -1736.74, z = 28.70},
+    {x = 144.34, y = 6642.69, z = 30.96}
+}
