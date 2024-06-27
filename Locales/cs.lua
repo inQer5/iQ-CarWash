f009b815990339bc19ee88c25e64add7dced812c
inQer5 <145898779+inQer5@users.noreply.github.com>
2024-06-27 15:56:02 +0200

    Add files via upload

---
+++ Locales/cs.lua
@@ -0,0 +1,14 @@
+Locales = {
+    ['car_wash'] = 'Automyčka',
+    ['standard'] = 'Standardní',
+    ['luxury'] = 'Luxusní',
+    ['price'] = 'Cena: $',
+    ['error_vehicle'] = 'Musíte být ve vozidle.',
+    ['error_money'] = 'Nemáte dostatek peněz.',
+    ['success'] = 'Úspěch',
+    ['success_wash'] = 'Vaše vozidlo bylo umyto.',
+    ['open_menu'] = 'Otevřít menu automyčky',
+    ['closed'] = 'Otevřeno je od 12:00 do 18:00',
+    ['washing'] = 'Probíhá mytí vozidla...',
+    ['error'] = 'Chyba'
+}
---
+++ Locales/en.lua
@@ -0,0 +1,14 @@
+Locales = {
+    ['car_wash'] = 'Car Wash',
+    ['standard'] = 'Standard',
+    ['luxury'] = 'Luxury',
+    ['price'] = 'Price: $',
+    ['error_vehicle'] = 'You must be in a vehicle.',
+    ['error_money'] = 'You don\'t have enough money.',
+    ['success'] = 'Success',
+    ['success_wash'] = 'Your vehicle has been washed.',
+    ['open_menu'] = 'Open car wash menu',
+    ['closed'] = 'Open from 12:00 to 18:00',
+    ['washing'] = 'Washing vehicle...',
+    ['error'] = 'Error'
+}
